b61da1589f14065169fe8dcfb3595354d88d43ca
ivanburda1986 <burda.ivan@gmail.com>
2021-02-16 01:19:11 +0100

    styling of the habit creation form

--- src/components/HabitTemplate/HabitTemplate.js
+++ src/components/HabitTemplate/HabitTemplate.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 
+//Component
+import HabitIconSelection from '../UI/HabitIconSelection/HabitIconSelection';
+
 //Style
 import classes from './HabitTemplate.module.css';
 
@@ -10,15 +13,17 @@ import readingIcon from '../../media/icons/reading.png';
 import programmingIcon from '../../media/icons/programming.png';
 import guitarIcon from '../../media/icons/guitar.png';
 import alarmclockIcon from '../../media/icons/alarmclock.png'
+import questionmark from '../../media/icons/questionmark.svg';
 
 
 class HabitTemplate extends Component {
   state={
-    selectedIcon: "readingIcon",
+    selectedIcon: null,
+    selectedIconImage: questionmark,
   }
 
-  handleIconSelection = (event)=>{
-    this.setState({selectedIcon:event.target.value})
+  handleIconSelection = (clickedIcon)=>{
+    this.setState({selectedIcon: clickedIcon.target.value, selectedIconImage:clickedIcon.target.nextSibling.children[0].src});
   };
 
   render(){
@@ -27,14 +32,14 @@ class HabitTemplate extends Component {
   
         <div className={classes.HabitTemplateTop}>
           <div className={classes.HabitTemplateTopLeft}>
-            <img src={alarmclockIcon}/>
+            <img src={this.state.selectedIconImage}/>
           </div>
           <div className={classes.HabitTemplateTopRight}>
             <form>
               <label htmlFor="HabitTemplateTitle"></label>
-              <input type="text" id="HabitTemplateTitle" name="HabitTemplateTitle" placeholder="Habit name"></input>
+              <input type="text" className={classes.HabitTemplateTitle} name="HabitTemplateTitle" placeholder="Habit name"></input>
               <label htmlFor="HabitTemplateDescription"></label>
-              <input type="text" id="HabitTemplateDescription" name="HabitTemplateDescription" placeholder="Description"></input>
+              <input type="text" className={classes.HabitTemplateDescription} name="HabitTemplateDescription" placeholder="Description"></input>
             </form>
           </div>
         </div>
@@ -42,15 +47,12 @@ class HabitTemplate extends Component {
         <div className={classes.HabitTemplateBottom}>
           <div className={classes.HabitTemplateBottomLeft}>
               
-            <input type="radio" id="sportIcon" name="iconSelection" value="sportIcon" onClick={this.handleIconSelection} />
-            <label htmlFor="sportIcon" className={classes.HabitIconSelection}><img src={sportIcon} alt="Sport icon" className={this.state.selectedIcon === "sportIcon" ? classes.selectedIcon : null}/></label>
-  
-            <input type="radio" id="sleepIcon" name="iconSelection" value="sleepIcon" onClick={this.handleIconSelection}/>
-            <label htmlFor="sleepIcon" className={classes.HabitIconSelection}><img src={sleepIcon} alt="Sleep icon" className={this.state.selectedIcon === "sleepIcon" ? classes.selectedIcon : null}/></label>
-  
-            <input type="radio" id="readingIcon" name="iconSelection" value="readingIcon" onClick={this.handleIconSelection}/>
-            <label htmlFor="readingIcon" className={classes.HabitIconSelection}><img src={readingIcon} alt="Reading icon" className={this.state.selectedIcon === "readingIcon" ? classes.selectedIcon : null}/></label>
-  
+          <HabitIconSelection iconImage={sportIcon} iconName="sportIcon" selectedIcon={this.state.selectedIcon} clicked={()=>this.handleIconSelection} />
+          <HabitIconSelection iconImage={sleepIcon} iconName="sleepIcon" selectedIcon={this.state.selectedIcon} clicked={()=>this.handleIconSelection} />
+          <HabitIconSelection iconImage={readingIcon} iconName="readingIcon" selectedIcon={this.state.selectedIcon} clicked={()=>this.handleIconSelection} />
+          <HabitIconSelection iconImage={programmingIcon} iconName="programmingIcon" selectedIcon={this.state.selectedIcon} clicked={()=>this.handleIconSelection} />
+          <HabitIconSelection iconImage={guitarIcon} iconName="guitarIcon" selectedIcon={this.state.selectedIcon} clicked={()=>this.handleIconSelection} />
+          <HabitIconSelection iconImage={alarmclockIcon} iconName="alarmclockIcon" selectedIcon={this.state.selectedIcon} clicked={()=>this.handleIconSelection} />
           </div>
   
           <div className={classes.HabitTemplateBottomRight}>
---
+++ src/components/UI/HabitIconSelection/HabitIconSelection.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import classes from './HabitIconSelection.module.css';
+
+
+const habitIconSelection = (props) =>{
+
+  return(
+    <React.Fragment>
+      <input type="radio" id={props.iconName} name="iconSelection" value={props.iconName} onClick={props.clicked()}/>
+      <label htmlFor={props.iconName} className={classes.HabitIconSelection}><img src={props.iconImage} alt="Icon" className={props.selectedIcon === props.iconName ? classes.selectedIcon : null}/></label>
+    </React.Fragment>
+  );
+}
+
+export default habitIconSelection;
