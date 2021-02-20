ebe8b2394aceb4e14d5d119d3258c49067e6bcfc
ivanburda1986 <burda.ivan@gmail.com>
2021-02-20 18:22:48 +0100

    implemented writing a new habit to the firebase db

--- src/containers/SetupHabits/SetupHabits.js
+++ src/containers/SetupHabits/SetupHabits.js
@@ -1,5 +1,19 @@
 import React, { Component } from 'react';
 
+
+//Firebase
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase";
+// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
+// import * as firebase from "firebase/app"
+
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import "firebase/analytics";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
 //Axios
 import axios from '../../axios-habits';
 
@@ -18,6 +32,23 @@ import programmingIcon from '../../media/icons/programming.png';
 import guitarIcon from '../../media/icons/guitar.png';
 import alarmclockIcon from '../../media/icons/alarmclock.png'
 
+// TODO: Replace the following with your app's Firebase project configuration
+// For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
+const  firebaseConfig = {
+  apiKey: "AIzaSyBERXVHlBmqzrOnUEFhfjji2k1GZ7pXfYU",
+  authDomain: "habits-6349a.firebaseapp.com",
+  databaseURL: "https://habits-6349a-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "habits-6349a",
+  storageBucket: "habits-6349a.appspot.com",
+  messagingSenderId: "491187493233",
+  appId: "1:491187493233:web:e7d0032961a21b581b7ed0",
+  measurementId: "G-7NE3EC8LYV"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+
 
 
 class SetupHabits extends Component {
@@ -32,10 +63,23 @@ class SetupHabits extends Component {
     updatedHabits.push(newHabit);
     this.setState({habits: updatedHabits});
 
-    //Send the habit to the server
-    axios.post('/habits.json', newHabit)
-      .then(response=>console.log(response))
-      .catch(error=>console.log(error));
+    const myhabit = {
+      title: "my new habit"
+    }
+
+  this.writeUserData(newHabit);
+  }
+
+  writeUserData= (newHabit)=> {
+    firebase.database().ref('habits/' + newHabit.id).set({
+      id : newHabit.id,
+      icon : newHabit.icon,
+      title : newHabit.title,
+      subtitle : newHabit.subtitle,
+      streak : newHabit.streak,
+      lastStreakUpdateTime : newHabit.lastStreakUpdateTime,
+      lastButOnStreakUpdateTime: newHabit.lastButOnStreakUpdateTime,
+    });
   }
 
   deleteHabitHandler = (clickedHabit) => {
