a068ea8716b0c082b7806317a89f805b8ebcc6ad
ivanburda1986 <burda.ivan@gmail.com>
2021-03-19 23:56:29 +0100

    Implement theme

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {Switch, Route, BrowserRouter, Redirect} from 'react-router-dom';
-
+import {ThemeProvider} from './contexts/theme';
 
 import Layout from './hoc/Layout/Layout';
 
@@ -10,18 +10,31 @@ import SetupHabits from './containers/SetupHabits/SetupHabits';
 import classes from './App.module.css';
 
 class App extends Component {
+  state={
+    theme: 'dark',
+    toggleTheme: ()=>{
+      this.setState(({theme})=>({
+        theme: theme === 'dark' ? 'light' : 'dark'
+      }))
+    }
+  }
+
   render(){
     return (
-      <div className={classes.App}>
-        <Layout>
-          <Switch>
-            <Route path="/today" exact component={TodayHabits}/>
-            <Route path="/setup" exact component={SetupHabits}/>
-            <Route path="/" exact component={TodayHabits}/>
-            <Redirect from="*" to="/"/>
-          </Switch>
-        </Layout>
-      </div>
+      <ThemeProvider value={this.state}>
+        <div className={this.state.theme}>
+          <div className={classes.App}>
+          <Layout>
+            <Switch>
+              <Route path="/today" exact component={TodayHabits}/>
+              <Route path="/setup" exact component={SetupHabits}/>
+              <Route path="/" exact component={TodayHabits}/>
+              <Redirect from="*" to="/"/>
+            </Switch>
+          </Layout>
+          </div>
+        </div>
+      </ThemeProvider>
     )
   }
 }
