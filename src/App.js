f51550644189ec34bb296b2c817d735a9bcde4ea
ivanburda1986 <burda.ivan@gmail.com>
2021-03-28 21:23:29 +0200

    removed the theme

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import {Switch, Route, BrowserRouter, Redirect} from 'react-router-dom';
-import {ThemeProvider} from './contexts/theme';
 
 import Layout from './hoc/Layout/Layout';
 
@@ -10,19 +9,9 @@ import SetupHabits from './containers/SetupHabits/SetupHabits';
 import classes from './App.module.css';
 
 class App extends Component {
-  state={
-    theme: 'dark',
-    toggleTheme: ()=>{
-      this.setState(({theme})=>({
-        theme: theme === 'dark' ? 'light' : 'dark'
-      }))
-    }
-  }
 
   render(){
     return (
-      <ThemeProvider value={this.state}>
-        <div className={this.state.theme}>
           <div className={classes.App}>
           <Layout>
             <Switch>
@@ -33,8 +22,6 @@ class App extends Component {
             </Switch>
           </Layout>
           </div>
-        </div>
-      </ThemeProvider>
     )
   }
 }
